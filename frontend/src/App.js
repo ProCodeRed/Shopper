b8aa9f4115ee1c9d792fa2df0488e02f58dcbe0f
ProCodeRed <yadav.vipinkumar@outlook.com>
2023-01-29 20:14:32 +0530

    added header footer and bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,15 @@
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
 function App() {
   return (
     <div className="App">
-        Welcome to Shoppers
+      <Header />
+      <Container>
+        <main className="py-3">Welcome to Shoppers</main>
+      </Container>
+      <Footer />
     </div>
   );
 }
