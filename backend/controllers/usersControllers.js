b0c0a7433c6a9f54f9ed69dd61b8c91edb6c4014
ProCodeRed07 <yadav.vipinkumar@outlook.com>
2024-09-30 00:52:48 +0530

    jwt token added

--- backend/controllers/usersControllers.js
+++ backend/controllers/usersControllers.js
@@ -1,5 +1,6 @@
 import asyncHandler from 'express-async-handler'
 import User from "../db/models/userModel.js";
+import generateToken from '../utils/generateToken.js';
 
 
 // @desc: auth user & get token
@@ -17,7 +18,7 @@ export const authUser = asyncHandler(async (req, res) => {
             name: user.name,
             email: user.email,
             isAdmin: user.isAdmin,
-            token: null,
+            token: generateToken(user._id),
         })
     }else{
         res.status(401)
