16bc101570f19f514e0973aa751ff4bcd6521f5a
ProCodeRed07 <yadav.vipinkumar@outlook.com>
2024-10-05 13:57:01 +0530

    create user login reducer and added to that in store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,11 +3,13 @@ import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { productDetailsReducer, productListReducer } from "./reducers/productReducers";
 import { cartReducer } from "./reducers/cartReducers";
+import { userLoginReducer } from "./reducers/userReducers";
 
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails :productDetailsReducer,
     cart: cartReducer,
+    userLogin: userLoginReducer,
 });
 
 const cartItemsFromLoaclstorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
