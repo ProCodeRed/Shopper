c8439560b2b2a2d68153142a18cf9ce7a4595cca
ProCodeRed07 <yadav.vipinkumar@outlook.com>
2024-10-06 18:11:43 +0530

    added decimal upto 2 points

--- frontend/src/pages/PlaceOrderPage.js
+++ frontend/src/pages/PlaceOrderPage.js
@@ -76,7 +76,7 @@ const PlaceOrderPage = () => {
                             </Link>
                           </Col>
                           <Col md={4}>
-                            {item.qty} x ${item.price} = ${item.qty * item.price}
+                            {item.qty} x ${item.price} = ${addDecimals(item.qty * item.price)}
                           </Col>
                         </Row>
                       </ListGroup.Item>
